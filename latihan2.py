39ef1382c2807ae9a62982ff0f683c5f326f823a
Agung Nugroho <mail@chung.web.id>
2019-10-21 08:55:26 +0700

    initial repository

---
+++ latihan2.py
@@ -0,0 +1,7 @@
+num = int(input("masukkan angka:"))
+if num >= 0:
+    print(num, "bilangan positif")
+else:
+    print(num, "bilangan negatif")
+
+print("ini diluar pernyataan if")
---
+++ main.py
@@ -0,0 +1,3 @@
+num = input("masukkan angka:")
+if (int(num) > 0):
+    print(num, "adalah bilangan positif")
